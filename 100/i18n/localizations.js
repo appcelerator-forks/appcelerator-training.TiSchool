1209c50fa527afe807f95488c4d63d3427a6ee40
Tim Poulsen <tim@skypanther.com>
2012-08-27 17:11:08 -0400

    Initial outline and lesson template

---
+++ 100/i18n/localizations.js
@@ -0,0 +1,22 @@
+/*
+	Localization strings file. To translate, copy the entire "en-US" branch, paste in as new member of the object, 
+	set its name to be the two-letter language code, and translate each string.
+	As shown, strings can include simple HTML.
+*/
+String.toLocaleString({
+	"en-US": {
+		"%l_filetitle": "TITLE",
+		"%l_lessontitle": "TITLE",
+		"%l_subtitle": "SUBTITLE",
+
+		"%l_qa_slide": "Q&amp;A",
+		"%l_slidenote_qa": "",
+
+		"%l_lab": "Lab Objectives",
+		"%l_labobjective1": "Foo",
+		"%l_labobjective2": "Bar",
+		"%l_labobjective3": "Baz",
+		"%l_slidenote_lab": "",
+
+	},
+});
